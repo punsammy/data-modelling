3cd29f7bd3b40241ca60db51ebdaea0ad57cb594
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-18 14:59:40 -0400

    create model for event

--- ex4/events/db/migrate/20170418185506_create_hosts.rb
+++ ex4/events/db/migrate/20170418185506_create_hosts.rb
@@ -1,6 +1,7 @@
 class CreateHosts < ActiveRecord::Migration[5.0]
   def change
     create_table :hosts do |t|
+      t.string :name
 
       t.timestamps
     end
--- ex4/events/db/migrate/20170418185511_create_guests.rb
+++ ex4/events/db/migrate/20170418185511_create_guests.rb
@@ -1,6 +1,7 @@
 class CreateGuests < ActiveRecord::Migration[5.0]
   def change
     create_table :guests do |t|
+      t.string :name
 
       t.timestamps
     end
