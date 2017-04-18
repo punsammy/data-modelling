eaf9e98b66cfbdcfc095fddf4115cb206997f606
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-18 14:02:36 -0400

    generate models

---
+++ ex1/store/db/migrate/20170418175917_create_orders.rb
@@ -0,0 +1,9 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+      t.date :date
+      t.integer :order_number
+      t.timestamps
+    end
+  end
+end
