32aa555a8b4f5137bf0eec83b14ffcfccdacf695
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-18 15:48:17 -0400

    create models and table

--- ex6/twitter/db/migrate/20170418194223_create_tweets.rb
+++ ex6/twitter/db/migrate/20170418194223_create_tweets.rb
@@ -1,6 +1,8 @@
 class CreateTweets < ActiveRecord::Migration[5.0]
   def change
     create_table :tweets do |t|
+      t.string :content
+
 
       t.timestamps
     end
---
+++ ex6/twitter/db/migrate/20170418194804_create_tweets_users.rb
@@ -0,0 +1,6 @@
+class CreateTweetsUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tweets_users do |t|
+    end
+  end
+end
