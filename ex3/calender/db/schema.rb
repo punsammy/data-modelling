84acd84654b34c9865742b6c5358c8cdf7a2853e
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-18 14:49:05 -0400

    add model for appointment

--- ex3/calender/db/schema.rb
+++ ex3/calender/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418183952) do
+ActiveRecord::Schema.define(version: 20170418184621) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer  "patient_id"
+    t.integer  "doctor_id"
+    t.date     "appointment"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
