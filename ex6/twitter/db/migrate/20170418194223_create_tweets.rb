8ac8bef1e561a21afb483cb3b544d7f5713c3fd0
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-19 09:43:19 -0400

    add associations

--- ex6/twitter/db/migrate/20170418194223_create_tweets.rb
+++ ex6/twitter/db/migrate/20170418194223_create_tweets.rb
@@ -2,6 +2,7 @@ class CreateTweets < ActiveRecord::Migration[5.0]
   def change
     create_table :tweets do |t|
       t.string :content
+      t.integer :user_id
 
 
       t.timestamps
--- ex6/twitter/db/migrate/20170418194804_create_tweets_users.rb
+++ ex6/twitter/db/migrate/20170418194804_create_tweets_users.rb
@@ -1,6 +1,8 @@
 class CreateTweetsUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :tweets_users do |t|
+      t.integer :user_id
+      t.integer :tweet_id
     end
   end
 end
